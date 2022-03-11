18a856d50bf3faec2248222f6ab7992b76fcd13f
ChristianZaccaria <christian.zaccaria.cz@gmail.com>
2022-03-11 18:19:43 +0000

    Submenu created for listing all notes, active notes, or archived notes.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -31,7 +31,7 @@ fun mainMenu() : Int {
          > --------------------------------
          > | NOTE MENU
          > |   1) Add a note              |
-         > |   2) List all notes          |
+         > |   2) List notes          |
          > |   3) Update a note           |
          > |   4) Delete a note           |
          > |   5) Archive a note
@@ -80,7 +80,40 @@ fun addNote(){
 
 fun listNotes(){
     //logger.info { "listNotes() function invoked" }
+    if (noteAPI.numberOfNotes() > 0) {
+        val option = readNextInt("""
+                  > --------------------------------
+                  > |   1) View ALL notes          |
+                  > |   2) View ACTIVE notes       |
+                  > |   3) View ARCHIVED notes     |
+                  > --------------------------------
+          > ==>>""".trimMargin(">"))
+
+
+        when (option) {
+            1 -> listAllNotes()
+            2 -> listActiveNotes()
+            3 -> listArchivedNotes()
+            else -> println("Invalid option entered $option")
+        }
+
+
+        }
+    else {
+        println("Option Invalid - No Notes Stored")
+    }
+}
+
+fun listAllNotes() {
     println(noteAPI.listAllNotes())
+}
+
+fun listActiveNotes() {
+    println(noteAPI.listActiveNotes())
+}
+
+fun listArchivedNotes() {
+    println(noteAPI.listArchivedNotes())
 }
 
 fun updateNote(){
